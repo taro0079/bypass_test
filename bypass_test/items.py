dbe9648715beceb38e07184e67c1015d275f9cce
Taro Morita <morita0079@gmail.com>
2022-05-19 23:37:33 +0900

    first commit

--- bypass_test/items.py
+++ bypass_test/items.py
@@ -9,4 +9,5 @@ import scrapy
 class BypassTestItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    url = scrapy.Field()
+    title = scrapy.Field()
